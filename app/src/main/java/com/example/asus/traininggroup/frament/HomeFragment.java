f98b6c35573127e2aa8eabca19102f6b88a00212
Alice <alice@gmail.com>
2017-09-21 17:27:15 +0800

    Button

--- app/src/main/java/com/example/asus/traininggroup/frament/HomeFragment.java
+++ app/src/main/java/com/example/asus/traininggroup/frament/HomeFragment.java
@@ -26,15 +26,4 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    public void test(){
-        LinearLayout llMain = (LinearLayout) view.findViewById(R.id.mainbutton);
-        LinearLayout llTeacherShow = (LinearLayout) view.findViewById(R.id.teachershow);
-        LinearLayout llBar = (LinearLayout) view.findViewById(R.id.bar);
-        TextView tvMain = (TextView) view.findViewById(R.id.tv_main_button);
-        TextView tvTeacherShow = (TextView) view.findViewById(R.id.tv_teachershow);
-        TextView tvBar = (TextView) view.findViewById(R.id.tv_bar);
-        llBar.addView(tvBar);
-        llMain.addView(tvMain);
-        llTeacherShow.addView(tvTeacherShow);
-    }
 }
